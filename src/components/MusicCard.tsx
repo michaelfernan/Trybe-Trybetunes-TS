73d92269d924d9d06c5b49303dd2526516439006
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-08-27 18:59:48 -0300

    Requsito 5 Imagem

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import checkedHeartImage from '../images/checked_heart.png';
+import emptyHeartImage from '../images/empty_heart.png';
 
 interface MusicCardProps {
   trackName: string;
   previewUrl: string;
+  trackId: number;
 }
 
-function MusicCard({ trackName, previewUrl }: MusicCardProps) {
+function MusicCard({ trackName, previewUrl, trackId }: MusicCardProps) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavoriteToggle = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <div>
       <h3>{trackName}</h3>
@@ -16,6 +25,17 @@ function MusicCard({ trackName, previewUrl }: MusicCardProps) {
         <code>audio</code>
         .
       </audio>
+      <label data-testid={ `checkbox-music-${trackId}` }>
+        <input
+          type="checkbox"
+          checked={ isFavorite }
+          onChange={ handleFavoriteToggle }
+        />
+        <img
+          src={ isFavorite ? checkedHeartImage : emptyHeartImage }
+          alt="favorite"
+        />
+      </label>
     </div>
   );
 }
